a72aff372109ac623b9f6576b7c4f2d0efb20759
GenericEarthling <sharon.tender@icloud.com>
2018-11-07 11:06:50 -0500

    practiced algorithms

---
+++ src/practice/RemoveDuplicates.java
@@ -0,0 +1,46 @@
+/**
+ * input:
+ *  number test cases
+ *  number 1-10power18
+ * output:
+ *  print number after removing consecutive digits
+ */
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tender
+ */
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        // get number
+        Scanner in = new Scanner(System.in);
+        Long numEntered = in.nextLong();
+        List<Long> list = new ArrayList<>();
+        list.
+        char[] charArray = str.toCharArray();
+        int length = charArray.length;
+        int index = 0;
+        while (index < length-1) {
+            char current = charArray[index];
+            char next = charArray[index+1];
+            if (current == next) {
+                // remove from array
+                charArray[index] = ;
+            }
+        } 
+        // get charAt index, compare to charAt index+1
+                // if match, remove from string
+        // loop through chars until you find a match
+        // get index number        
+        // remove char at that index        
+        // repeat loop with same char until reaching end of string
+        // change search char to second number
+    }
+    
+}
